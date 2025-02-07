31aa046c70a80eb6b319ca62c2724bb7d5eb0fad
Suraj Giri <155833064+surajixcp@users.noreply.github.com>
2025-02-07 18:24:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+document.querySelectorAll('.code').forEach((input, index, inputs) => {
+    input.addEventListener('input', (e) => {
+        if (e.target.value.length === 1 && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    });
+
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace') {
+            if (input.value === '') {
+                if (index > 0) {
+                    inputs[index - 1].focus();
+                }
+            }
+        }
+    });
+});
+
